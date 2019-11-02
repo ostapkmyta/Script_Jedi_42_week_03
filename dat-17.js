576d2580076af03cea1d12732798f0ee34ff1038
Ostap Kmyta <54151907+ostapkmyta@users.noreply.github.com>
2019-11-03 01:57:49 +0200

    Create dat-17.js

---
+++ dat-17.js
@@ -0,0 +1,26 @@
+//first task https://www.codewars.com/kata/training-js-number-29-methods-of-arrayobject-concat-and-join/train/javascript
+
+function bigToSmall(arr){
+  let result = [];
+  for(const a of arr){
+   result= result.concat(a);
+   result.sort((a, b) => b - a);
+  }
+    return result.join('>');
+}
+
+//second task  http://www.codewars.com/kata/573156709a231dcec9000ee8
+
+const head = n => +String(n)[0];
+const tail = n => +String(n).split('').slice(-1)[0];
+const tailAndHead = a => {
+   let result = [];
+   a.reduce ((p,c) => {
+    result.push(tail(p) + head(c));
+    return p = c;
+   });
+   return result.reduce((p,c) => p*c);
+}
+
+
+//third task 
